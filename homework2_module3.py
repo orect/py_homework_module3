7e2541a0b500fac85908ba886194691e615e626f
orect <iamorest228@gmail.com>
2025-07-27 22:48:40 +0300

    Update homework2_module3.py

--- homework2_module3.py
+++ homework2_module3.py
@@ -1,11 +1,19 @@
 import random
 
-def get_numbers_ticket(min, max, quantity):
-    result = set()
+def get_numbers_ticket(min_value, max_value, quantity):
 
-    numbers = random.sample(range(min, max + 1), quantity)
+    if (
+        not isinstance(min_value, int)
+        or not isinstance(max_value, int)
+        or not isinstance(quantity, int)
+        or min_value < 1
+        or max_value > 1000
+        or min_value > max_value
+        or quantity <= 0
+        or quantity > (max_value - min_value + 1)
+    ):
+        return []
+
+    
+    numbers = random.sample(range(min_value, max_value + 1), quantity)
     return sorted(numbers)
-
-print(get_numbers_ticket(1, 100, 10))
-    
-    
