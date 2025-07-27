a12daab8be63026dbfae372822903162a2d0cf8d
orect <iamorest228@gmail.com>
2025-07-27 22:47:57 +0300

    Update howe_work1_module3.py

--- howe_work1_module3.py
+++ howe_work1_module3.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    now = datetime.now()
-
-    diff = now - date
-    return diff.days
-
-date = '2021-12-31'
-result = get_days_from_today(datetime.strptime(date, '%Y-%m-%d'))
-print(f"Days from today to {date}: {result} days")
+    try:
+        target_date = datetime.strptime(date, "%Y-%m-%d").date()
+        
+        today = datetime.today().date()
+        
+        delta = today - target_date
+        return delta.days
+    except ValueError:
+        
+        return "Invalid date format. Please use 'YYYY-MM-DD'."
